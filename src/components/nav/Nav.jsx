1e940f9d84b20fae3106e4102a6375ead6e2ec22
Hamza KHRIBICH <hamza.khribich@gmail.com>
2022-05-18 18:18:56 +0200

    Nav component implemented

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { MdOutlineScreenSearchDesktop } from 'react-icons/md'
+import { BiMessageSquareDetail } from 'react-icons/bi'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' id='home-navlink' className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')} ><AiOutlineHome /></a>
+      <a href='#about' id='about-navlink' className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
+      <a href='#experience' id='experience-navlink' className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')}><BiBook /></a>
+      <a href='#projects' id='projects-navlink' className={activeNav === '#projects' ? 'active' : ''} onClick={() => setActiveNav('#projects')}><MdOutlineScreenSearchDesktop /></a>
+      <a href='#contact' id='contact-navlink' className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
