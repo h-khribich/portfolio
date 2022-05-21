15a7f55e8122eff8acc6c30766140c76acc3a778
Hamza KHRIBICH <hamza.khribich@gmail.com>
2022-05-21 18:31:08 +0200

    Projects component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>
+    <div className='main-wrapper'>
       <Header />
       <Nav />
       <About />
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
 import './projects.css'
+import Reservia from '../../assets/portfolio-project-images/reservia.png'
+import Ohmyfood from '../../assets/portfolio-project-images/ohmyfood.png'
+import Fisheye from '../../assets/portfolio-project-images/fisheye.png'
+import Hrnet from '../../assets/portfolio-project-images/hrnet.png'
+import Sportsee from '../../assets/portfolio-project-images/sportsee.png'
+import Kasa from '../../assets/portfolio-project-images/kasa.png'
 
 const Projects = () => {
   return (
-    <section id='projects'></section>
+    <section id='projects'>
+      <h5>Portfolio</h5>
+      <h2>Projets</h2>
+
+      <div className="container projects__container">
+        <article className='projects__item'>
+          <div className="projects__item-img">
+            <img src={Kasa} alt="" />
+          </div>
+          <h3>Application SPA de location immobilière avec React et React Router</h3>
+          <div className="projects__item-cta">
+            <a href='https://github.com/h-khribich/HamzaKhribich_11_19122021' className='btn' rel='noreferrer' target='_blank'>Github</a>
+            <a href='https://hamzakhribich-11-19122021.pages.dev/' className='btn btn-primary' rel='noreferrer' target='_blank'>Live demo</a>
+          </div>
+        </article>
+        <article className='projects__item'>
+          <div className="projects__item-img">
+            <img src={Sportsee} alt="" />
+          </div>
+          <h3>Dashboard d'analyse de performance sportive avec React et Recharts</h3>
+          <div className="projects__item-cta">
+            <a href='https://github.com/h-khribich/HamzaKhribich_12_11012022' className='btn' rel='noreferrer' target='_blank'>Github</a>
+            <a href='https://hamzakhribich-12-11012022.pages.dev/' className='btn btn-primary' rel='noreferrer' target='_blank'>Live demo</a>
+          </div>
+        </article>
+        <article className='projects__item'>
+          <div className="projects__item-img">
+            <img src={Ohmyfood} alt="" />
+          </div>
+          <h3>Site de presentations de restaurants avec HTML/CSS et Sass</h3>
+          <div className="projects__item-cta">
+            <a href='https://github.com/h-khribich/HamzaKhribich_3_25052021' className='btn' rel='noreferrer' target='_blank'>Github</a>
+            <a href='https://h-khribich.github.io/HamzaKhribich_3_25052021/' className='btn btn-primary' rel='noreferrer' target='_blank'>Live demo</a>
+          </div>
+        </article>
+        <article className='projects__item'>
+          <div className="projects__item-img">
+            <img src={Reservia} alt="" />
+          </div>
+          <h3>Site statique de reservations de vacances avec HTML/CSS</h3>
+          <div className="projects__item-cta">
+            <a href='https://github.com/h-khribich/HamzaKhribich_2_10052021' className='btn' rel='noreferrer' target='_blank'>Github</a>
+            <a href='https://h-khribich.github.io/HamzaKhribich_2_10052021/' className='btn btn-primary' rel='noreferrer' target='_blank'>Live demo</a>
+          </div>
+        </article>
+        <article className='projects__item'>
+          <div className="projects__item-img">
+            <img src={Hrnet} alt="" />
+          </div>
+          <h3>Conversion d'une application jQuery en React et Redux</h3>
+          <div className="projects__item-cta">
+            <a href='https://github.com/h-khribich/HamzaKhribich_14_20032022' className='btn' rel='noreferrer' target='_blank'>Github</a>
+            <a href='https://hamzakhribich-14-20032022.pages.dev/' className='btn btn-primary' rel='noreferrer' target='_blank'>Live demo</a>
+          </div>
+        </article>
+        <article className='projects__item'>
+          <div className="projects__item-img">
+            <img src={Fisheye} alt="" />
+          </div>
+          <h3>Site dynamique de pages de photographes en JS vanilla</h3>
+          <div className="projects__item-cta">
+            <a href='https://github.com/h-khribich/HamzaKhribich_6_14072021' className='btn' rel='noreferrer' target='_blank'>Github</a>
+            <a href='https://h-khribich.github.io/HamzaKhribich_6_14072021/' className='btn btn-primary' rel='noreferrer' target='_blank'>Live demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
